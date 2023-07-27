562b7617ec51350ac89a8111cac9f3820eacb24d
Ahmet Orbay <ahmetorbay06@gmail.com>
2023-07-27 12:06:11 +0300

    restore js file (merge)

---
+++ Turkai/wwwroot/js/AddedLocalStorage.js
@@ -0,0 +1,22 @@
+﻿function AddedLocalStorage(data, Id) {
+    var LocalData = JSON.parse(localStorage.getItem("Basket"));
+    if (LocalData) {
+        console.log(LocalData)
+        var existingItemIndex = LocalData.findIndex(function (item) {
+            return item.Id === Id;
+        });
+
+        if (existingItemIndex !== -1) {
+
+            LocalData[existingItemIndex].Count += data.Count;
+        } else {
+
+            LocalData.push(data);
+        }
+    } else {
+
+        LocalData = [data];
+    }
+    localStorage.setItem("Basket", JSON.stringify(LocalData));
+
+}
---
+++ Turkai/wwwroot/js/AddedSepet.js
@@ -0,0 +1,33 @@
+function AddedSepet(Id, Title) {
+    var count = document.getElementById("basketSize-" + Id).value
+    $("#errorText").text("");
+    $("#errorPopup").hide();
+    $.ajax({
+        url: "/Home/BasketCheck",
+        type: "POST",
+        data: {
+            basketDetail: [{
+                Id: Id,
+                Title: Title,
+                Count: count
+            }]
+        },
+        success: function (result) {
+            if (result.checkStock == false) {
+                $("#errorText").text(result.title + " stock quantity is not enough");
+                $("#errorPopup").show();
+            } else {
+                AddedLocalStorage({
+                    Id: Id,
+                    Title: Title,
+                    Count: parseInt(count)
+                }, Id)
+                console.log("Sonuç: " + result);
+            }
+        },
+        error: function (xhr, status, error) {
+
+            console.error("Hata: " + error);
+        }
+    });
+}
